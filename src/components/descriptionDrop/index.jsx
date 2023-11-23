e537a5c6b3cf873ebd6726bfac826096376490ce
boris blanche <borisblanche77gmail.com>
2023-11-23 14:13:07 +0100

    oomits accordion v2

--- src/components/descriptionDrop/index.jsx
+++ src/components/descriptionDrop/index.jsx
@@ -1,67 +1,86 @@
-import React from "react";
+// import React from "react";
 
-import styled from "styled-components";
-import { useParams } from 'react-router-dom';
-// import StarRating from '../rating';
+// import styled from "styled-components";
+// import { useParams } from 'react-router-dom';
+// // import StarRating from '../rating';
 
-const InfoLog = styled.div`
-    display:flex;
-    flex-direction:raw;
-    padding-bottom:40px;
-    padding-top:40px;`
-
-
-
-
-const DescriptionDrop = styled.select`
-padding-left:20px;
-padding-right:20px;
-border-radius:25px;
-padding :10px;
-width:30%;
-margin:auto;`
-
-const EquipmentDrop = styled.select`
-padding-left:20px;
-padding-right:20px;
-padding :10px;
-border-radius:15px;
-width:30%;
-margin:auto;
-`
-
-
-const OptionDescription = styled.option`
-
-display:flex;`
-
-
-const OptionEquipment = styled.option`
-
-display:flex;`
+// const InfoLog = styled.div`
+//     display:flex;
+//     flex-direction:raw;
+//     padding-bottom:40px;
+//     padding-top:40px;`
 
 
 
 
-function DescripCard({ appartements }){
-    const { id } = useParams();
+// const DescriptionDrop = styled.select`
+// padding-left:20px;
+// padding-right:20px;
+// border-radius:25px;
+// padding :10px;
+// width:30%;
+// margin:auto;`
+
+// const EquipmentDrop = styled.select`
+// padding-left:20px;
+// padding-right:20px;
+// padding :10px;
+// border-radius:15px;
+// width:30%;
+// margin:auto;
+// `
+
+
+// const OptionDescription = styled.option`
+
+// display:flex;`
+
+
+// const OptionEquipment = styled.option`
+
+// display:flex;`
+
+
+
+
+// function DescripCard({ appartements }){
+//     const { id } = useParams();
  
-    const logement = appartements.find((appartement)=>appartement.id.toString() === id);
+//     const logement = appartements.find((appartement)=>appartement.id.toString() === id);
   
   
-    return (
-    <InfoLog>
-    <DescriptionDrop>
-    <OptionDescription >description </OptionDescription>
-    <OptionDescription value="">{logement.description}</OptionDescription>
-    </DescriptionDrop>
-    <EquipmentDrop>
-    <OptionEquipment>equipements</OptionEquipment>
-    <OptionEquipment value="">{logement.equipments}</OptionEquipment>
-      </EquipmentDrop>
-      </InfoLog>
-    );
-}
+//     return (
+//     <InfoLog>
+//     <DescriptionDrop>
+//     <OptionDescription >description </OptionDescription>
+//     <OptionDescription value="">{logement.description}</OptionDescription>
+//     </DescriptionDrop>
+//     <EquipmentDrop>
+//     <OptionEquipment>equipements</OptionEquipment>
+//     <OptionEquipment value="">{logement.equipments}</OptionEquipment>
+//       </EquipmentDrop>
+//       </InfoLog>
+//     );
+// }
 
+
+// export default DescripCard;
+
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import Accordion from '../accordion';
+
+
+const DescripCard = ({appartements}) => {
+  const { id } = useParams();
+ 
+      const logement = appartements.find((appartement)=>appartement.id.toString() === id);
+  return (
+    <div>
+      <Accordion title="Description"content={logement.description} />
+      <Accordion title="Equipements" content={logement.equipments} />
+    </div>
+  );
+};
 
 export default DescripCard;
--- src/pages/apropos/apropos.jsx
+++ src/pages/apropos/apropos.jsx
@@ -5,15 +5,26 @@ import Banner from "../../components/banner"
 import Footer from "../../components/footer"
 // import styled from "styled-components"
 import Accordion from "../../components/accordion"
+import { useParams } from 'react-router-dom';
 
 
 
-function Apropos(){
+
+
+function Apropos() {
+    const accordionData = [
+            { title: 'Fiabilité', content: 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.' },
+            { title: 'Respect', content: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou deperturbation du voisinage entraînera une exclusion de notre plateforme.' },
+            { title: 'Service', content: ' Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N hésitez pas à nous contacter si vous avez la moindre question.' },
+            { title: 'Sécurité', content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l hôte qu au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." },
+          ];
     return (
         <div>
             <NavHeader />
             <Banner />
-            <Accordion/>
+     {accordionData.map((item, index) => (
+        <Accordion key={index} title={item.title} content={item.content} />
+      ))}
         <Footer/>
         </div>
     )
